cdb442e88f9355b17a770e20df9d833448d5d76d
Dan Green <danngreen1@gmail.com>
2022-03-12 15:27:04 -0800

    Simplified main2

--- src/main2.c
+++ src/main2.c
@@ -1,38 +1,30 @@
-#include "hardware.h"
 #include "formats.h"
+#include "hardware.h"
 
 void board_usb_initialize(void);
 void board_usb_activate(void);
 
-static void lowinitialize(void)
-{
+int main(void) {
+  global_disableIRQ();
+  cpu_initialize(); // в случае ARM - инициализация прерываний и контроллеров,
+                    // AVR - запрет JTAG
+
 #if WITHDEBUG
-	HARDWARE_DEBUG_INITIALIZE();
-	HARDWARE_DEBUG_SET_SPEED(DEBUGSPEED);
-	dbg_puts_impl_P(PSTR("Version " __DATE__ " " __TIME__ " 1 debug session starts.\n"));
+  HARDWARE_DEBUG_INITIALIZE();
+  HARDWARE_DEBUG_SET_SPEED(DEBUGSPEED);
+  dbg_puts_impl_P(
+      PSTR("Version " __DATE__ " " __TIME__ " 1 debug session starts.\n"));
 #endif /* WITHDEBUG */
 
-	hardware_timer_initialize(TICKS_FREQUENCY);
+  hardware_timer_initialize(TICKS_FREQUENCY);
 
-	cpu_initdone();			/* секция init (в которой лежит образ для загрузки в FPGA) больше не нужна */
+  board_usb_initialize(); // USB device and host support
+  dbg_puts_impl_P(PSTR("Most of hardware initialized.\n"));
 
-	board_usb_initialize();		// USB device and host support
-	dbg_puts_impl_P(PSTR("Most of hardware initialized.\n"));
+  global_enableIRQ();
+  board_usb_activate(); // USB device and host start
 
-}
-
-
-
-int main(void)
-{
-	global_disableIRQ();
-	cpu_initialize();		// в случае ARM - инициализация прерываний и контроллеров, AVR - запрет JTAG
-	lowinitialize();	/* вызывается при запрещённых прерываниях. */
-	global_enableIRQ();
-	board_usb_activate();		// USB device and host start
-
-	for (;;)
-	{
-	}
-	return 0;
+  for (;;) {
+  }
+  return 0;
 }
